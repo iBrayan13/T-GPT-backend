41b01d7590d86f733ba483b8bc280a02f4a5d9d9
brayanbarret0 <brayanbarreto1130@gmail.com>
2023-09-19 06:40:05 -0400

    Fixing request /chat

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Flying to GPT
+
+## Summary
+
+**Flying to GPT** is a website that gives people from countries where chat gpt is not available, **instant and free access** to questions and answers. **Simply** type in a question and get an instant response from the chat gpt. Find the answers you need quickly and easily!
--- src/routes/ChatRouters.py
+++ src/routes/ChatRouters.py
@@ -7,15 +7,15 @@ service = ChatService()
 @router.get("/get-answer")
 async def get_answer(request: Request) -> dict:
     data = await request.json()
-    if not len(data) == 1 or "question" not in data or data["question"] != str:
+    if not len(data) == 1 or "question" not in data or not type(data["question"]) == str:
         raise HTTPException(
             status_code= status.HTTP_400_BAD_REQUEST,
             detail= "Invalid request",
             headers= {"content-type": "application/json"}
         )
     
-    ans = await service.make_question(data["question"])
+    ans =service.make_question(data["question"])
 
     return {
-        "data": ans
+        "answer": ans
     }
