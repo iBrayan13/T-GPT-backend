213165c7aa562fa4dcd0e8868d447efe8785e62d
brayanbarret0 <brayanbarreto1130@gmail.com>
2023-09-21 18:12:21 -0400

    Adding requirements.txt

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from src.routers import AuthRouters, ChatRouters
 from decouple import config
