e69318f16bc3b625b411ec7ca397461989980e2f
brayanbarret0 <brayanbarreto1130@gmail.com>
2023-12-10 18:57:37 -0400

    Updating ChatServices

--- src/services/ChatServices.py
+++ src/services/ChatServices.py
@@ -1,10 +1,7 @@
-import openai
+from requests import api
 from decouple import config
 
-openai.api_key = config("OPENAI_API_KEY")
-
 class ChatService:
-    GPT_ENGINE = config("GPT_ENGINE")
 
     @classmethod
     def make_question(cls, question: str) -> str:
@@ -17,14 +14,25 @@ class ChatService:
             str: answer by chatgpt.
         """
         try:
-            completion = openai.Completion.create(
-                engine= cls.GPT_ENGINE,
-                prompt= question,
-                max_tokens= 2048
+            res = api.post(
+                url= "https://api.openai.com/v1/chat/completions",
+                headers= {
+                    "Authorization": f"Bearer {config('OPENAI_API_KEY')}",
+                    "Content-Type": "application/json",
+                },
+                json= {
+                    "model": "gpt-3.5-turbo",
+                    "messages": [{"role": "user", "content": question}],
+                }
             )
-            
-            answer = completion.choices[0].text
-            return f"{answer} \n\nObtenido por #T-GPT"
+            if res.status_code == 200:
+                data = res.json()
+                answer = data["choices"][0]["message"]["content"]
+
+                return f"{answer} \n\nObtenido por #T-GPT"
+            else:
+                print("Something wrong with response")
+                return ""
             
         except Exception as ex:
             print(ex)
